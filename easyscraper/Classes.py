4c3f1c8eb97a78058710fe2ebfdc2c3ef59fdc39
yishai <yishairasowsky@gmail.com>
2020-12-20 19:55:43 +0200

    create classes file split story into phrases print text on image get only large images remove files of words and images, because i have them in the classes file anyway

--- easyscraper/Classes.py
+++ easyscraper/Classes.py
@@ -1,3 +1,4 @@
+import re
 import os
 import spacy
 import requests  
@@ -5,10 +6,10 @@ import numpy
 import matplotlib.pyplot as plt
 
 from io import BytesIO
-from PIL import Image
 from bs4 import BeautifulSoup
-from spacy.lang.en import English
+from PIL import Image, ImageFont, ImageDraw 
 from textblob import TextBlob
+from spacy.lang.en import English
 
 class WordManager:
   def __init__(self):
@@ -18,20 +19,70 @@ class WordManager:
     with open('story.txt') as file:
       self.story = file.read()
 
+  def clean_story(self):
+    self.story = re.sub(r'\n+', ' ', self.story)#.strip()
+    self.story = re.sub(r' +', ' ', self.story)#.strip()
+
   def get_nouns(self):
-    blob = TextBlob(self.story)
-    self.nouns = [token[0] for token in blob.tags if token[1]=='NN']
+    self.noun_lists = []
+    chunks = []
+    remainder = self.story
+    chunk_chars = 60
+    while len(remainder) > chunk_chars:
+        chunk = remainder[0:chunk_chars]
+        last_space = chunk.rfind(' ')
+        chunk = chunk[:last_space]
+        chunks.append(chunk)
+        chunk_size = len(chunk)
+        remainder = remainder[chunk_size +1:]
+    chunks.append(remainder)
+
+    sentences = chunks
+    for sentence in sentences:
+      blob = TextBlob(sentence)
+      wanted_POS = {'NN',
+      # 'CD',
+      # 'NNP',
+      # 'VBG',
+      'NNS',
+      # 'VBN'
+      }
+      noun_list = [token[0] for token in blob.tags if token[1] in wanted_POS]
+      self.noun_lists.append((sentence,noun_list))
 
 class PictureManager:
-  def __init__(self,num_imgs,search_words):
+  def __init__(self,num_imgs,word_lists):
     self.num_imgs = num_imgs
-    self.search_words = search_words
+    self.word_lists = word_lists
 
-  def save_img(self,search_word,img_idx,img_url):
-    response = requests.get(img_url)
-    img = Image.open(BytesIO(response.content))
-    plt.imshow(img)
-    plt.savefig(f'imgs/{search_word}_{img_idx}.png')
+  def save_img(self,search_word,img_idx,img_url,sentence, sentence_idx):
+    text = sentence
+    response = None
+    try:
+      response = requests.get(img_url)
+      img = Image.open(BytesIO(response.content))
+      width, height = img.size
+      draw = ImageDraw.Draw(img)
+      font = ImageFont.truetype("arial.ttf", 12)
+
+      text_width, text_height = font.getsize(text)
+      image_width, image_height = (width, height)
+      left_edge = image_width - 1.0*text_width - 5
+      right_edge = image_width - 0.0*text_width + 5, # right side, more positive farther right
+      draw.rectangle((
+          left_edge,
+          image_height, 
+          right_edge,
+          image_height - text_height
+          ), fill='white')
+      draw.text((left_edge, image_height - text_height), text, fill=(0, 0, 0), font=font)
+
+      img.save(f'imgs/phrase_{sentence_idx}_word_{search_word}_{img_idx}.png')
+
+      pass
+
+    except:
+      print("Could not get response...")
 
 
   def limit_imgs(self,all_imgs):
@@ -53,7 +104,13 @@ class PictureManager:
 
       search_url = f'https://images.search.yahoo.com/search/images;_ylt=AwrExdrB_MlfwyYAsv6LuLkF;_ylc=X1MDOTYwNTc0ODMEX3IDMgRmcgMEZ3ByaWQDc0cuQS5iMU9ScGl0aVlTNFlUUnBrQQRuX3N1Z2cDMTAEb3JpZ2luA2ltYWdlcy5zZWFyY2gueWFob28uY29tBHBvcwMwBHBxc3RyAwRwcXN0cmwDBHFzdHJsAzYEcXVlcnkDcmFtYmFuBHRfc3RtcAMxNjA3MDcyOTY2?fr2=sb-top-images.search&p={search_word}&ei=UTF-8&iscqry=&fr=sfp'
 
-      htmldata =  requests.get(search_url).text
+      htmldata = ''
+
+      try:
+        htmldata =  requests.get(search_url).text
+      
+      except: 
+        pass
 
       soup = BeautifulSoup(htmldata, 'html.parser')  
 
@@ -73,15 +130,24 @@ class PictureManager:
 
     self.make_img_dir()
 
-    for search_word in self.search_words:
+    for sentence_idx, sentence_tuple in enumerate(self.word_lists):
 
-      self.get_html(search_word)
+      sentence = sentence_tuple[0]
 
-      self.limit_imgs(self.all_imgs)
+      word_list = sentence_tuple[1]
 
-      for img_idx,img_url in enumerate(self.img_urls[:self.num_imgs]):
-          
-          self.save_img(search_word,img_idx,img_url)
+      for search_word in word_list:
+
+        self.get_html(search_word)
+
+        self.limit_imgs(self.all_imgs)
+
+        for img_idx,img_url in enumerate(self.img_urls[:self.num_imgs]):
+            
+            self.save_img(search_word,img_idx,img_url, sentence, sentence_idx)
+
+
+
 
 
 def main():
