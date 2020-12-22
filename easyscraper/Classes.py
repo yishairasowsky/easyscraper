2671e65aff783a77a65198d6ce6535ee1cb2237d
yishai <yishairasowsky@gmail.com>
2020-12-22 18:40:53 +0200

    align text and rectagnle in middle

--- easyscraper/Classes.py
+++ easyscraper/Classes.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 from io import BytesIO
 from bs4 import BeautifulSoup
 from PIL import Image, ImageFont, ImageDraw 
+from datetime import datetime
 from textblob import TextBlob
 from spacy.lang.en import English
 
@@ -23,45 +24,35 @@ class WordManager:
     self.story = re.sub(r'\n+', ' ', self.story)#.strip()
     self.story = re.sub(r' +', ' ', self.story)#.strip()
 
-  def get_nouns(self):
+  def get_nouns(self, phrase_length = 35):
     self.noun_lists = []
+    sentences = []
+    sentence = ''
+    for i in range(0,len(self.story)):
+      sentence += self.story[i]
+      if sentence[-1] == '.' or (len(sentence)>phrase_length and self.story[i+1] == ' ') or (sentence[-1] == ',' and len(sentence)>20):
+        if sentence[0] == ' ':
+          sentence = sentence[1:]
+        sentences.append(sentence)
+        sentence = ''
+      else:
+        continue
 
-    sentences = self.story.split('.')
-
-    for sentence in sentences:
-
-    chunks = []
-    remainder = self.story
-    chunk_chars = 50
-    while len(remainder) > chunk_chars:
-        chunk = remainder[0:chunk_chars]
-        last_space = chunk.rfind(' ')
-        chunk = chunk[:last_space]
-        chunks.append(chunk)
-        chunk_size = len(chunk)
-        remainder = remainder[chunk_size +1:]
-    chunks.append(remainder)
-
-    sentences = chunks
     for sentence in sentences:
       blob = TextBlob(sentence)
-      wanted_POS = {'NN',
-      # 'CD',
-      # 'NNP',
-      # 'VBG',
-      'NNS',
-      # 'VBN'
-      }
+      wanted_POS = {'NN','CD','NNP','VBG','NNS','VBN'}
       noun_list = [token[0] for token in blob.tags if token[1] in wanted_POS]
       self.noun_lists.append((sentence,noun_list))
 
+
 class PictureManager:
+
   def __init__(self,num_imgs,word_lists):
     self.num_imgs = num_imgs
     self.word_lists = word_lists
 
-  def save_img(self,sentence, sentence_idx,search_word=None,img_idx=None,img_url=None):
-    text = sentence
+  def save_img(self,sentence, sentence_idx,word_idx=None,img_idx=None,img_url=None):
+    text = sentence    
     response = None
     img = None
     try:
@@ -73,21 +64,37 @@ class PictureManager:
 
       width, height = img.size
       draw = ImageDraw.Draw(img)
-      font = ImageFont.truetype("arial.ttf", 12)
+      font_size = 10
+      font = ImageFont.truetype("arial.ttf", font_size)
 
       text_width, text_height = font.getsize(text)
       image_width, image_height = (width, height)
-      left_edge = image_width - 1.0*text_width - 10
-      right_edge = image_width - 0.0*text_width + 10 # right side, more positive farther right
+
+      left_edge = image_width - text_width - 5 - 0.5*(image_width - text_width)
+      right_edge = left_edge + text_width + 10 # right edge, more positive = farther right
+
+      top_edge = 0.7*image_height # more positive = farther down
+      bottom_edge = top_edge + text_height + 2.0
+
       draw.rectangle((
           left_edge,
-          image_height, 
+          bottom_edge, # bottom edge
           right_edge,
-          image_height - text_height
+          top_edge # top edge
           ), fill='white')
-      draw.text((left_edge + 5, image_height - text_height), text, fill=(0, 0, 0), font=font)
 
-      img.save(f'imgs/phrase_{sentence_idx}_word_{search_word}_{img_idx}.png')
+      draw.text(
+        (
+          left_edge + 5, # left edge
+          top_edge # top edge
+          ), 
+        text, fill=(0, 0, 0), font=font)
+
+      sentence_str = str(sentence_idx).zfill(3)
+
+      word_str = str(word_idx).zfill(3)
+
+      img.save(f'imgs/sent_{sentence_str}_word_{word_str}.png')
 
       pass
 
@@ -148,7 +155,7 @@ class PictureManager:
 
       word_list = sentence_tuple[1]
 
-      for search_word in word_list:
+      for word_idx, search_word in enumerate(word_list):
 
         self.get_html(search_word)
 
@@ -156,10 +163,7 @@ class PictureManager:
 
         for img_idx,img_url in enumerate(self.img_urls[:self.num_imgs]):
             
-            self.save_img(search_word=search_word,img_idx=img_idx,img_url=img_url, sentence=sentence, sentence_idx=sentence_idx)
-
-
-
+            self.save_img(word_idx=word_idx,img_idx=img_idx,img_url=img_url, sentence=sentence, sentence_idx=sentence_idx)
 
 
 def main():
